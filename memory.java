b4f68ee2e9b5e7470752adddafd36a24e23d5dbc
Daniel Tang <33831500+Ramposa@users.noreply.github.com>
2022-07-07 14:03:56 +0100

    Add files via upload

---
+++ memory.java
@@ -0,0 +1,14 @@
+public class memory {
+    public static void main(String[] args) 
+    {
+      int i=1; 
+    
+      i = silly(i);
+  }
+
+  static int silly(int locali)
+    {
+      locali = 3;
+      return locali;
+    } 
+}
